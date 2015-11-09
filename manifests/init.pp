6bc0036711649f74088cf4a4f5523033985d791e
Udo Waechter <udo@maketank.net>
2015-11-09 17:11:19 +0100

    more packages in jessie

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,11 @@
-class rkhunter ($ensure = "present") {
-    package {
-        "rkhunter" :
-            ensure => $ensure,
-    }
-    file {
-        "/etc/rkhunter.conf" :
-            content => template("rkhunter/rkhunter.conf.erb"),
-            owner => root,
-            mode => 0600,
-            before => Package["rkhunter"],
-            ensure => $ensure,
-    }
+class rkhunter ($ensure = 'present') {
+  package { ['rkhunter','prelink']: ensure => $ensure, }
+
+  file { '/etc/rkhunter.conf':
+    content => template('rkhunter/rkhunter.conf.erb'),
+    owner   => root,
+    mode    => 0600,
+    before  => Package['rkhunter'],
+    ensure  => $ensure,
+  }
 }
